30ba40ccbd1d2ce6e4c0877435cf9f8c4060b648
leovinh <thevinhduong@gmail.com>
2016-03-13 01:36:32 +0700

    Push navigation and passing data to Detail scene

---
+++ FlicksMovie/FlicksMovie/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  FlicksMovie
+//
+//  Created by The Vinh Duong on 3/13/16.
+//  Copyright © 2016 Cititech. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: NSDictionary!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = movie["title"] as! String
+         titleLabel.text = title
+        
+        let overview = movie["overview"]
+        overviewLabel.text = overview as! String
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

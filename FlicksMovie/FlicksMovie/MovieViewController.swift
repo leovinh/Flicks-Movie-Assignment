602ce172dc642f60b2b7d3470058239dc5e51273
leovinh <thevinhduong@gmail.com>
2016-03-12 18:38:42 +0700

    finish first scene

--- FlicksMovie/FlicksMovie/MovieViewController.swift
+++ FlicksMovie/FlicksMovie/MovieViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -49,10 +50,18 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w342"
+        
+        let imageURL = NSURL(string: baseUrl + posterPath)
+        
+        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        
+        cell.posterView.setImageWithURL(imageURL!)
         
         
         print("row \(indexPath.row)")
---
+++ FlicksMovie/Pods/Target Support Files/Pods-FlicksMovie/Pods-FlicksMovie-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FlicksMovieVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FlicksMovieVersionString[];
+

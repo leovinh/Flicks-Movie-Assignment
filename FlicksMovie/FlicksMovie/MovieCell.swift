70a026c83b9422053a08209a670edc0cf27ac7e1
leovinh <thevinhduong@gmail.com>
2016-03-12 15:58:51 +0700

    Custom cells

---
+++ FlicksMovie/FlicksMovie/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  FlicksMovie
+//
+//  Created by The Vinh Duong on 3/12/16.
+//  Copyright © 2016 Cititech. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FlicksMovie/FlicksMovie/MovieViewController.swift
+++ FlicksMovie/FlicksMovie/MovieViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -46,12 +44,17 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
         
-        cell.textLabel?.text = title
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        
         print("row \(indexPath.row)")
         return cell
         
